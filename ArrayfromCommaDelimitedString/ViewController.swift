57e01b24acab31d1ff1120079486363704c5b60b
Daniel Washington <danielvertigo@hotmail.com>
2021-06-02 19:20:19 -0300

    Files

--- ArrayfromCommaDelimitedString/ViewController.swift
+++ ArrayfromCommaDelimitedString/ViewController.swift
@@ -5,6 +5,28 @@
 //  Created by Daniel Washington Ignacio on 02/06/21.
 //
 
+/*
+ Write a function that turns a comma-delimited list into an array of strings.
+
+ Examples
+
+ toArray("watermelon, raspberry, orange")
+ ➞ ["watermelon", "raspberry", "orange"]
+
+ toArray("x1, x2, x3, x4, x5")
+ ➞ ["x1", "x2", "x3", "x4", "x5"]
+
+ toArray("a, b, c, d")
+ ➞ ["a", "b", "c", "d"]
+
+ toArray("")
+ ➞ []
+ Notes
+
+ Return an empty array for an empty string.
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +34,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(self.toArray("watermelon, raspberry, orange"))
+        print(self.toArray("x1, x2, x3, x4, x5"))
+        print(self.toArray("a, b, c, d"))
+        print(self.toArray(""))
+    }
+    
+    func toArray(_ str: String) -> [String] {
+        return str.components(separatedBy: ", ")
     }
 
 
